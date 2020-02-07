0ee57299dad3a88e84394abd1e8f5fff160808f1
Dario Tecchia <dariotecchia@gmail.com>
2020-02-07 14:32:31 +0100

    added mul example

---
+++ test_files/product.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define nop(){;}
+
+/********************* Declarations ****************/
+int result;
+int b = 0;
+int a = 0;
+
+/********************* Functions declaration *******/
+
+int mul(int x, int y);
+
+/********************* Functions definition  *******/
+int main(){
+scanf("%d", &a);
+scanf("%d", &b);
+result = mul(a, b);
+printf("%s", "la moltiplicazione risulta: ");
+printf("%d", result);
+}
+
+int mul(int x, int y){
+{
+int toReturn = 0;
+{
+int i;
+for(i = 0; i < y; i++){
+toReturn = toReturn + x;
+}
+}
+return toReturn;
+}
+}
